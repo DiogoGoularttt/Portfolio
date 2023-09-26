4496aeb4a4b13e00eb9b12b290ad27487135ad59
Diogo Santos Goulart <diogosgoulart@icloud.com>
2023-09-26 13:48:52 -0300

    :rocket: Add modals in projects

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,44 +1,99 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import "../styles/components/ProjectsPage.sass";
-
 import Image1 from '../img/LTIP.png';
 import Image2 from '../img/ProjetoElétrico.png';
 import Image3 from '../img/ProjetoEstrutural.png';
-import Image4 from '../img/CoberturaEmTreliça.png'
+import Image4 from '../img/CoberturaEmTreliça.png';
+
+Modal.setAppElement('#root');
 
 const ProjectsContainer = () => {
-    return (
-        <section className="projects-container">
-            <h2>Projetos</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
-                officia consectetur mollitia dolorem eius inventore omnis ex quo natus earum
-                iusto at quis commodi sint odio ad recusandae totam doloribus!
-            </p>
-            {/* <Link to="/projetos" className="btn">
-                Ver Projetos
-            </Link> */}
-            <div className="gallery-container">
-                <div className="gallery-items">
-                    <img src={Image1} alt="LTIP - Inspeção Predial" />
-                </div>
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
 
-                <div className="gallery-items">
-                    <img src={Image2} alt="Projeto Elétrico" />
-                </div>
+  const openModal = (project) => {
+    setSelectedProject(project);
+    setModalIsOpen(true);
+  };
 
-                <div className="gallery-items">
-                    <img src={Image3} alt="Projeto Estrutural" />
-                </div>
+  const closeModal = () => {
+    setSelectedProject(null);
+    setModalIsOpen(false);
+  };
 
-                <div className="gallery-items">
-                    <img src={Image4} alt="Cobertura em Treliça" />
-                </div>
-            </div>
+  const projects = [
+    { id: 1, image: Image1, description: 'Descrição do Projeto 1' },
+    { id: 2, image: Image2, description: 'Descrição do Projeto 2' },
+    { id: 3, image: Image3, description: 'Descrição do Projeto 3' },
+    { id: 4, image: Image4, description: 'Descrição do Projeto 4' },
+  ];
 
-        </section>
-    )
-}
+  return (
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+        officia consectetur mollitia dolorem eius inventore omnis ex quo natus earum
+        iusto at quis commodi sint odio ad recusandae totam doloribus!
+      </p>
+
+      <div className="gallery-container">
+        {projects.map((project) => (
+          <div className="gallery-items" key={project.id}>
+            <img
+              src={project.image}
+              alt={project.description}
+              onClick={() => openModal(project)}
+            />
+          </div>
+        ))}
+      </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Descrição do Projeto"
+        style={{
+          content: {
+            maxWidth: '60%',
+            margin: 'auto',
+            backgroundColor: '#1d1d1d',
+            border: 'none',
+            position: 'relative',
+            height: '90%', // Defina uma altura fixa para todos os modais
+          },
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          },
+        }}
+      >
+        {selectedProject && (
+          <>
+            <button
+              onClick={closeModal}
+              style={{
+                position: 'absolute',
+                top: '10px',
+                right: '10px',
+                background: 'none',
+                border: 'none',
+                cursor: 'pointer',
+                fontSize: '24px',
+                color: 'white',
+              }}
+            >
+              &times;
+            </button>
+            <h3>Descrição do Projeto</h3>
+            <p>{selectedProject.description}</p>
+          </>
+        )}
+      </Modal>
+    </section>
+  );
+};
 
 export default ProjectsContainer;
+
+
--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -10,15 +10,17 @@ const SocialNetworks = () => {
     return (
         <section id='social-networks'>
             {socialNetworks.map((network) => (
-                <><a href="https://www.linkedin.com/in/c%C3%A9saro-trujillo-corr%C3%AAa-779117160/" className='social-btn' id={network.name} key={network.name} target="_blank">
-                    {network.icon}
-                </a><a href="https://www.linkedin.com/in/c%C3%A9saro-trujillo-corr%C3%AAa-779117160/" style={{ color: 'white', background: 'none', textDecoration: 'none', }} target="_blank">
+                <div key={network.name}>
+                    <a href="https://www.linkedin.com/in/c%C3%A9saro-trujillo-corr%C3%AAa-779117160/" className='social-btn' id={network.name} target="_blank">
+                        {network.icon}
+                    </a>
+                    <a href="https://www.linkedin.com/in/c%C3%A9saro-trujillo-corr%C3%AAa-779117160/" className='social-link' style={{ color: 'white', background: 'none', textDecoration: 'none', }} target="_blank">
                         LinkedIn
-                    </a></>
-
+                    </a>
+                </div>
             ))}
         </section>
-    )
+    );
 };
 
 export default SocialNetworks
