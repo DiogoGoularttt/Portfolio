36cc56c40d7fae5a384a194cd4fcb3eefc9e6a82
Diogo Santos Goulart <diogosgoulart@icloud.com>
2023-09-26 04:08:08 -0300

    :wrench: Add changes to the side bar

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -10,12 +10,14 @@ const SocialNetworks = () => {
     return (
         <section id='social-networks'>
             {socialNetworks.map((network) => (
-                <a href="https://www.linkedin.com/in/c%C3%A9saro-trujillo-corr%C3%AAa-779117160/" className='social-btn' id={network.name} key={network.name} target="_blank">
+                <><a href="https://www.linkedin.com/in/c%C3%A9saro-trujillo-corr%C3%AAa-779117160/" className='social-btn' id={network.name} key={network.name} target="_blank">
                     {network.icon}
-                </a>
-                
+                </a><a href="https://www.linkedin.com/in/c%C3%A9saro-trujillo-corr%C3%AAa-779117160/" style={{ color: 'white', background: 'none', textDecoration: 'none', }} target="_blank">
+                        LinkedIn
+                    </a></>
+
             ))}
-        </section>      
+        </section>
     )
 };
 
