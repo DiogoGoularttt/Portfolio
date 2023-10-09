c0813c38970efdee7d447e83854abf7e4919d309
João Pedro Viana <pitter@meza.digital>
2023-10-09 14:02:49 -0300

    Preparando para deploy 2.0

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
-// import Button from "@material-ui/core/Button";
 
 // Estilos
 import '../styles/components/ProjectsPage.sass';
